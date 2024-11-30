101721ad56b6bee436b336d2f60f6733ed8aca31
Osama-El-Dawli <saidosama283@gmail.com>
2024-11-30 21:20:05 +0200

    ..

--- Program.cs
+++ Program.cs
@@ -11,22 +11,10 @@ namespace GymDesktop
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
-//<<<<<<< HEAD
-            Application.Run(new AddMember());
-//=======
-            //Application.Run(new Payment());
-//>>>>>>> sara
-=======
-<<<<<<< Updated upstream
-            Application.Run(new ViewMembers());
-=======
+
 
             Application.Run(new AddMember());
 
-
->>>>>>> Stashed changes
->>>>>>> osama
         }
     }
 }
