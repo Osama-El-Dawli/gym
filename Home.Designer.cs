b41b369bafc7465535e9ed6bcedad43f980306ff
tahoun <mohamedashrf016@gmail.com>
2024-12-02 21:32:26 +0200

    This is home

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -28,92 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
-            menuStrip1 = new MenuStrip();
-            addMemberToolStripMenuItem = new ToolStripMenuItem();
-            toolStripMenuItem2 = new ToolStripMenuItem();
-            viewMemberToolStripMenuItem = new ToolStripMenuItem();
-            updateDeleteToolStripMenuItem = new ToolStripMenuItem();
-            paymentToolStripMenuItem = new ToolStripMenuItem();
-            menuStrip1.SuspendLayout();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
-            // menuStrip1
+            // button1
             // 
-            menuStrip1.BackColor = Color.LightGray;
-            menuStrip1.ImageScalingSize = new Size(20, 20);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { addMemberToolStripMenuItem, viewMemberToolStripMenuItem, updateDeleteToolStripMenuItem, paymentToolStripMenuItem });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.RenderMode = ToolStripRenderMode.Professional;
-            menuStrip1.Size = new Size(1335, 28);
-            menuStrip1.TabIndex = 0;
-            menuStrip1.Text = "menuStrip1";
-            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            button1.BackColor = Color.DarkOrange;
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(1034, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(203, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Payment\r\n";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
             // 
-            // addMemberToolStripMenuItem
+            // button2
             // 
-            addMemberToolStripMenuItem.BackColor = Color.Silver;
-            addMemberToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem2 });
-            addMemberToolStripMenuItem.Image = (Image)resources.GetObject("addMemberToolStripMenuItem.Image");
-            addMemberToolStripMenuItem.Name = "addMemberToolStripMenuItem";
-            addMemberToolStripMenuItem.Size = new Size(135, 24);
-            addMemberToolStripMenuItem.Text = "Add Member ";
-            addMemberToolStripMenuItem.Click += addMemberToolStripMenuItem_Click;
+            button2.BackColor = Color.DarkOrange;
+            button2.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.Location = new Point(744, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(241, 60);
+            button2.TabIndex = 1;
+            button2.Text = "Update/Delete";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
-            // toolStripMenuItem2
+            // button3
             // 
-            toolStripMenuItem2.Name = "toolStripMenuItem2";
-            toolStripMenuItem2.Size = new Size(83, 26);
-            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
+            button3.BackColor = Color.DarkOrange;
+            button3.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            button3.Location = new Point(459, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(241, 60);
+            button3.TabIndex = 2;
+            button3.Text = "View Members";
+            button3.UseVisualStyleBackColor = false;
             // 
-            // viewMemberToolStripMenuItem
+            // button4
             // 
-            viewMemberToolStripMenuItem.Image = (Image)resources.GetObject("viewMemberToolStripMenuItem.Image");
-            viewMemberToolStripMenuItem.Name = "viewMemberToolStripMenuItem";
-            viewMemberToolStripMenuItem.Size = new Size(135, 24);
-            viewMemberToolStripMenuItem.Text = "View Member";
+            button4.BackColor = Color.DarkOrange;
+            button4.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            button4.Location = new Point(186, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(222, 60);
+            button4.TabIndex = 3;
+            button4.Text = "Add Member";
+            button4.UseVisualStyleBackColor = false;
             // 
-            // updateDeleteToolStripMenuItem
+            // label1
             // 
-            updateDeleteToolStripMenuItem.BackColor = Color.Silver;
-            updateDeleteToolStripMenuItem.Image = (Image)resources.GetObject("updateDeleteToolStripMenuItem.Image");
-            updateDeleteToolStripMenuItem.Name = "updateDeleteToolStripMenuItem";
-            updateDeleteToolStripMenuItem.Size = new Size(142, 24);
-            updateDeleteToolStripMenuItem.Text = "Update/Delete";
+            label1.AutoSize = true;
+            label1.BackColor = Color.Black;
+            label1.Font = new Font("Segoe UI", 120F, FontStyle.Bold);
+            label1.ForeColor = Color.DarkOrange;
+            label1.Location = new Point(258, 225);
+            label1.Name = "label1";
+            label1.Size = new Size(907, 265);
+            label1.TabIndex = 4;
+            label1.Text = "Big Gym";
             // 
-            // paymentToolStripMenuItem
+            // label2
             // 
-            paymentToolStripMenuItem.Image = (Image)resources.GetObject("paymentToolStripMenuItem.Image");
-            paymentToolStripMenuItem.Name = "paymentToolStripMenuItem";
-            paymentToolStripMenuItem.Size = new Size(99, 24);
-            paymentToolStripMenuItem.Text = "Payment";
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 28.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Tomato;
+            label2.Location = new Point(526, 517);
+            label2.Name = "label2";
+            label2.Size = new Size(372, 62);
+            label2.TabIndex = 5;
+            label2.Text = "No Pain No Gain";
+            label2.Click += label2_Click;
             // 
             // Home
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackColor = SystemColors.ActiveCaptionText;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1335, 692);
-            Controls.Add(menuStrip1);
-            MainMenuStrip = menuStrip1;
+            ClientSize = new Size(1441, 743);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Home";
             Text = "Home";
             WindowState = FormWindowState.Maximized;
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private MenuStrip menuStrip1;
-        private ToolStripMenuItem addMemberToolStripMenuItem;
-        private ToolStripMenuItem viewMemberToolStripMenuItem;
-        private ToolStripMenuItem updateDeleteToolStripMenuItem;
-        private ToolStripMenuItem paymentToolStripMenuItem;
-        private ToolStripMenuItem toolStripMenuItem2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
     }
 }
--- Home.cs
+++ Home.cs
@@ -29,7 +29,27 @@ namespace GymDesktop
 
         private void addMemberToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
